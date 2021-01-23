b04774e31677d35b620f111d1e5fb95019581264
destroyeddesman <65105529+destroyeddesman@users.noreply.github.com>
2021-01-23 00:57:03 -0800

    most of backend
    added functions: get searches from google trend api, finance api for the ticker, gets the moving average for the trend api, applies the trend to the stock price to predicate.
    
    need to work on a bit of the math and also the google trend api only has data from 01 to 15 so i used that to predict 1/22/2021 stock price. It actually works pretty well. 
    Need to workout functionality with the whole project and bit of statistics.

---
+++ interest.py
@@ -0,0 +1,158 @@
+import pandas as pd
+
+from pytrends.request import TrendReq
+
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas_datareader import data
+import requests
+import yfinance as yf
+
+from pandas import DataFrame
+
+#input... can be array but need flask functionality
+stock_ticker = ["MRNA"]
+
+
+#nothing
+#stock_name = []
+
+#for x in stock_ticker:
+#    tick = yf.Ticker(x)
+#    stock_name.append(tick.info['shortName'])
+
+
+
+
+
+
+
+
+def get_searches(key_word):
+    pytrends = TrendReq(hl='en-US', tz=360)
+    pytrends.build_payload([key_word], cat=0, timeframe='2020-01-01 2021-01-15',  gprop='',geo='')
+    df = pytrends.interest_over_time()
+
+
+    print(df.head())
+
+    sns.set()
+    df['timestamp'] = pd.to_datetime(df.index)
+    sns.lineplot(df['timestamp'], df[key_word])
+
+    plt.title("Normalized Searches for {}".format(key_word))
+    plt.ylabel("Number of Searches")
+    plt.xlabel("Date")
+    plt.show()
+    plt.close()
+
+
+def get_finance(key_word):
+    df = yf.download(key_word, start='2020-01-01', end='2021-01-15')['Adj Close']
+    pd.plotting.register_matplotlib_converters()
+# Load the data
+
+# Set the style to seaborn for plotting
+    plt.style.use('seaborn')
+    fig, ax = plt.subplots(figsize=(12, 6))
+# Plot the cumulative returns fot each symbol
+    ax.plot(df)
+    ax.legend()
+    plt.title('Adjusted Close Price - {}'.format(key_word), fontsize=16)
+# Define the labels for x-axis and y-axis
+    plt.ylabel('Adjusted Close Price', fontsize=14)
+    plt.xlabel('Date', fontsize=14)
+    plt.show()
+    plt.close()
+
+
+def prediction(key_word):
+    pytrends = TrendReq(hl='en-US', tz=360)
+    pytrends.build_payload([key_word], cat=0, timeframe='2021-01-01 2021-01-15',  gprop='',geo='')
+    df = pytrends.interest_over_time()
+
+
+    std = pd.DataFrame.from_dict(df)
+
+
+    std['Moving Average'] = std[key_word].rolling(2).mean()
+    std[[key_word,'Moving Average']].plot(figsize=(10,4))
+    plt.grid(True)
+    plt.title(key_word +"Google Trends" ' Moving Averages')
+    plt.axis('tight')
+    plt.ylabel('Searches')
+    plt.show()
+
+
+
+def actual_prediction(key_word):
+        pytrends = TrendReq(hl='en-US', tz=360)
+        pytrends.build_payload([key_word], cat=0, timeframe='2021-01-01 2021-01-15',  gprop='',geo='')
+        df = pytrends.interest_over_time()
+        std = pd.DataFrame.from_dict(df)
+
+
+        std['Moving Average'] = std[key_word].rolling(2).mean()
+
+
+
+        close = float(std.loc['2021-01-15','Moving Average'])
+        l=[]
+        l.append(float(std.loc['2021-01-15','Moving Average']))
+        l.append(float(std.loc['2021-01-14','Moving Average']))
+
+
+
+
+        average =0
+        count = 0
+        for x in l:
+            average = average + x
+            count +=1
+
+        average  = average /count
+
+        final = close/average
+
+
+
+
+
+        df = yf.download(key_word, start='2021-01-01', end='2021-01-16')['Adj Close']
+        eat = pd.DataFrame.from_dict(df)
+        fire = float(eat.loc['2021-01-15','Adj Close'])
+
+        fire = fire*final
+
+        df1 = pd.DataFrame({"a":["2021-01-04","2021-01-05","2021-01-06","2021-01-07","2021-01-08","2021-01-11","2021-01-12",
+        "2021-01-13","2021-01-14","2021-01-15","2021-01-22"], "b":[float(eat.loc['2021-01-04','Adj Close']),float(eat.loc['2021-01-05','Adj Close']),float(eat.loc['2021-01-06','Adj Close']),
+        float(eat.loc['2021-01-07','Adj Close']),float(eat.loc['2021-01-08','Adj Close']),float(eat.loc['2021-01-11','Adj Close']),float(eat.loc['2021-01-12','Adj Close']),
+        float(eat.loc['2021-01-13','Adj Close']),float(eat.loc['2021-01-14','Adj Close']),float(eat.loc['2021-01-15','Adj Close']), fire]})
+
+
+
+        sns.set()
+
+        sns.lineplot(df1['a'], df1['b'])
+
+        plt.title("Predicted Stock Price {}".format(key_word))
+        plt.ylabel("Price")
+        plt.xlabel("Date")
+        plt.show()
+        plt.close()
+
+
+
+
+
+
+
+
+
+
+for x in stock_ticker:
+    get_searches(x)
+    get_finance(x)
+    prediction(x)
+    actual_prediction(x)
