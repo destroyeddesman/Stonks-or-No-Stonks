e9a9e5647035c2013f24fad09fcd929e7ca962e7
jeddyhuang <44849164+jeddyhuang@users.noreply.github.com>
2021-01-23 23:20:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,14 @@ app = Flask(__name__, template_folder='template',static_folder='template\static'
 def form():
     return render_template('form.html')
 
-@app.route('/data/', methods = ['POST', 'GET'])
+@app.route('/data', methods = ['POST', 'GET'])
 def data():
     if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+        return "The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
         form_data = request.form.to_dict()
         completer(form_data)
-
         return render_template('data.html')
-
 
 @app.after_request
 def add_header(response):
@@ -28,14 +26,11 @@ def add_header(response):
     return response
 
 def completer(named):
-
     ticker = str(named['Name'])
     interest.get_finance(ticker)
     interest.get_searches(ticker)
     interest.prediction(ticker)
     interest.actual_prediction(ticker)
-
-
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000)
